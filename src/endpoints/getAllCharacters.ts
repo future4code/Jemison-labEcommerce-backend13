5644e3717edf6380f700da63d6dbe542ed0672c4
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-12-02 19:15:37 -0300

    Final

--- src/endpoints/getAllCharacters.ts
+++ src/endpoints/getAllCharacters.ts
@@ -8,7 +8,20 @@ import connection from '../connection';
 export const getAllCharacters = async (req: Request, res: Response): Promise<void> => {
 
     try {
+
+        const { name, orderBy, orderType, page } = req.query
+
+        const resultsPerPage = 5 // Paginação
+        const ofsett = resultsPerPage * (Number(page) - 1) //Paginação
+        debugger 
+
+
         const characters: character[] = await connection("character")
+            .where("name", "LIKE", `%${name}%`) // filtrar por name 
+            .orderBy(orderBy as string || "name", orderType as string) // ordenação
+            .offset(ofsett) // paginação 
+
+
         res.send(characters)
     } catch (error) {
         res.status(500).send("Unexpected server error!")
